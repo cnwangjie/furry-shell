ef9fcd3eae5c31ffeaf29de1a5f538e0a11baf71
Wang Jie <doudou19758@gmail.com>
2017-05-04 13:17:54 +0800

    fix bootrun script for new version gnome-terminal

--- bootrun.sh
+++ bootrun.sh
@@ -2,15 +2,15 @@
 # 开机自启脚本
 
 # 开启redshift
-gnome-terminal -x zsh -c 'redshift -t 6000:4500' &
+gnome-terminal --window -- zsh -c 'redshift -t 6000:4500' &
 
 # 开启overture
 sudo sed -i '1i\nameserver 127.0.0.1' /etc/resolv.conf
-gnome-terminal -x zsh -c 'cd /home/wangjie/Downloads && sudo -S ./overture-linux-amd64 -v' &
+gnome-terminal --window -- zsh -c 'cd /home/wangjie/Downloads && sudo -S ./overture-linux-amd64 -v' &
 
 # 开启ss
 ss-qt5 &
 
 # 开发环境
-gnome-terminal -x zsh -c 'sudo -S mongod' &
-gnome-terminal -x zsh -c 'cd /home/wangjie/Workspace/adminMongo && npm run start' &
+gnome-terminal --window -- zsh -c 'sudo -S mongod' &
+gnome-terminal --window -- zsh -c 'cd /home/wangjie/Workspace/adminMongo && npm run start' &
