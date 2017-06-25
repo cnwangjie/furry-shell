7708b9ac2164f7adaa8a8cf9c47fe1a8b49cb675
Wang Jie <doudou19758@gmail.com>
2017-06-25 17:22:01 +0800

    fix path bug

--- upic.sh
+++ upic.sh
@@ -3,10 +3,17 @@
 user=$(whoami)
 filename=$(ls -t /home/${user}/Pictures | head -n 1)
 filepath="/home/${user}/Pictures/${filename}"
-lastuploadfile=$(cat .lastupload)
-if [ $filepath = $lastuploadfile ];then
+lastuploadpath="/home/${user}/.lastupload"
+
+if [ ! -d "${lastuploadpath}" ];then
+  touch ${lastuploadpath}
+fi
+
+lastuploadfile=$(cat "${lastuploadpath}")
+
+if [ "$filepath" = "$lastuploadfile" ];then
   echo "have uploaded!"
 else
   curl -F "smfile=@/home/${user}/Pictures/${filename}" -F "format=xml" https://sm.ms/api/upload | grep -Po 'https://[a-z0-9./]+.[a-z]+' | curl -X PUT -F c=@- http://fars.ee/a43bc24e-1250-406b-aa3d-890db4739fc7
-  echo "/home/${user}/Pictures/${filename}" > .lastupload
+  echo "/home/${user}/Pictures/${filename}" > ${lastuploadpath}
 fi
