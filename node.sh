04ac701f3c128c0906edb2b3cc565e51dab56943
Wang Jie <doudou19758@gmail.com>
2017-08-25 17:28:36 +0800

    update node.sh for more adaptation

--- node.sh
+++ node.sh
@@ -2,9 +2,52 @@
 
 # Tested in Debian
 clear
-# command -v wget >/dev/null 2>&1 || { echo >&2 "Please install wget first"; read ; exit 1; }
-apt-get update
-apt-get install -y wget gcc make
+_command_exist () {
+  command -v $1 >/dev/null 2>&1
+}
+
+if _command_exist wget; then
+  echo "Please install wget first"
+  read
+  exit 1
+fi
+
+_install_node () {
+  NODE_VERSION=`echo "$(wget https://nodejs.org/dist/index.json -O -)" | jq '.[0].version' | grep -o -e '[^"]*'`
+  wget https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz
+  tar -xvf node-$NODE_VERSION-linux-x64.tar.xz
+  mv node-$NODE_VERSION-linux-x64 /usr/local/node
+  rm /bin/node /bin/npm
+  ln -s /usr/local/node/bin/node /bin/node
+  ln -s /usr/local/node/bin/npm /bin/npm
+}
+
+_install_mongodb () {
+  if _command_exist mongod; then
+    echo "mongodb is installed in your system"
+    return
+  fi
+  wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.7.tgz
+  tar -zxvf mongodb-linux-x86_64-3.4.7.tgz
+  mkdir -p mongodb
+  cp -R -n mongodb-linux-x86_64-3.4.7/ mongodb
+  echo "export PATH=`pwd`/mongodb/bin:\$PATH" >> .bashrc
+  mkdir -p /data/db
+}
+
+_install_redis () {
+  wget http://download.redis.io/releases/redis-stable.tar.gz
+  tar -xvf redis-stable.tar.gz
+  mv redis-stable redis
+  cd redis
+  make distclean
+  make
+  cd utils
+  ./install_server.sh
+  cd ..
+  cd ..
+}
+
 clear
 echo "Install node.js ? (yes / no)"
 read install_node
@@ -14,13 +57,13 @@ echo "Install redis ? (yes / no)"
 read install_redis
 clear
 echo "You select to install following things:"
-if [ "$install_node" = "yes" ];then
+if [ "$install_node" != "no" ];then
   echo "node.js"
 fi
-if [ "$install_mongodb" = "yes" ];then
+if [ "$install_mongodb" != "no" ];then
   echo "mongodb"
 fi
-if [ "$install_redis" = "yes" ];then
+if [ "$install_redis" != "no" ];then
   echo "redis"
 fi
 echo ""
@@ -28,39 +71,18 @@ echo "press ENTER to continue | press CTRL+C to stop"
 read
 
 # install node
-if [ "$install_node" = "yes" ];then
-  wget https://nodejs.org/dist/v6.9.4/node-v6.9.4-linux-x64.tar.xz
-  tar -xvf node-v6.9.4-linux-x64.tar.xz
-  mv node-v6.9.4-linux-x64 /usr/local/node
-  rm /bin/node /bin/npm
-  ln -s /usr/local/node/bin/node /bin/node
-  ln -s /usr/local/node/bin/npm /bin/npm
+if [ "$install_node" != "no" ];then
+  _install_node
 fi
 
 # install mongodb
 if [ "$install_mongodb" = "yes" ];then
-  wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-3.4.1.tgz
-  tar -xvf mongodb-src-r3.4.1.tar.gz
-
-  mv mongodb-src-r3.4.1 /usr/local/mongodb
-  mkdir -p /data/db
-  rm /bin/mongod /bin/mongo
-  ln -s /usr/local/mongodb/bin/mongod /bin/mongod
-  ln -s /usr/local/mongodb/bin/mongo /bin/mongo
+  _install_mongodb
 fi
 
 # install redis
 if [ "$install_redis" = "yes" ];then
-  wget http://download.redis.io/releases/redis-3.2.9.tar.gz
-  tar -xvf redis-3.2.9.tar.gz
-  mv redis-3.2.6 redis
-  cd redis
-  make distclean
-  make
-  cd utils
-  ./install_server.sh
-  cd ..
-  cd ..
+  _install_redis
 fi
 
 exit 0;
